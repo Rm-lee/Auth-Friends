993c6d967e60a0ecc4a61b98fc4fd1a3dff05230
Roger Lee <rmlee0003@gmail.com>
2019-07-18 20:03:03 -0400

    added submit handler for login form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 class Login extends Component {
  constructor() {
   super();
   this.state = {
    username: '',
-   password: ''
+   password: '',
+   isLoading: false
   }
  }
  changeHandler = (e) => {
@@ -15,9 +16,17 @@ class Login extends Component {
   })
  }
  
+ submitHandler = (e) => {
+  e.preventDefault()
+  const { username, password } = this.state
+  axios.post('http://localhost:5000/',{username, password})
+  .then((response) => {
+   localStorage.setItem('response.data')
+  })
+ }
  render() {console.log(this.state.username)
   return (
-  <form >
+  <form onSubmit={this.submitHandler}>
    <input type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.changeHandler} /><br />
 				<input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.changeHandler} /><br />
    <button type="submit">Login</button>
