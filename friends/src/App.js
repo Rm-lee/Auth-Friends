fd8287da2068a858540e6bfcee2e48a9ac4cbb2f
Roger Lee <rmlee0003@gmail.com>
2019-11-03 20:07:44 -0500

    created submit and changehandlers for login added route to root app

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Login from './components/Login'
+import { Route } from 'react-router-dom'
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Route exact path="/login" component={Login} />
     </div>
   );
 }
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,11 +1,32 @@
 import React from "react";
-
+import axios from 'axios'
+import {useState} from 'react'
 function Login(props) {
   const [state, setState] = useState({
     username: "",
     password: "",
-    isLoading: false
+    
   });
+
+  function submitHandler(e){
+   e.preventDefault()
+   const { username, password } = state
+   axios.post('http://localhost:5000/api/login', {username,password})
+   .then((res) => {
+    localStorage.setItem('token', res.data.payload)
+    
+   })
+  }
+
+
+  function handleChange(e){
+   const value = e.target.value
+   setState({
+    ...state,
+    [e.target.name]: value
+   })
+  }
+
 
   return (
     <div>
@@ -25,6 +46,8 @@ function Login(props) {
          value={state.password}
          onChange={handleChange}
          />
+         <br />
+         <button type="submit">Login</button>
 
       </form>
     </div>
